93445f26917a29178ca2031abb3cfd304110ecf3
Goga <2doug077@gmail.com>
2021-05-07 13:03:45 -0300

    env var test 4

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO tb_sellers(name) VALUES ('Douglas');
-INSERT INTO tb_sellers(name) VALUES ('Flávia');
-INSERT INTO tb_sellers(name) VALUES ('Eric');
-INSERT INTO tb_sellers(name) VALUES ('Marcos');
-INSERT INTO tb_sellers(name) VALUES ('Luciane');
+INSERT INTO tb_sellers(name) VALUES ('Logan');
+INSERT INTO tb_sellers(name) VALUES ('Anakin');
+INSERT INTO tb_sellers(name) VALUES ('BarryAllen');
+INSERT INTO tb_sellers(name) VALUES ('Kal-El');
+INSERT INTO tb_sellers(name) VALUES ('Padme');
 
 INSERT INTO tb_sales(seller_id,visited,deals,amount,date) VALUES (5,83,66,5501.0,'2021-04-01');
 INSERT INTO tb_sales(seller_id,visited,deals,amount,date) VALUES (5,113,78,8290.0,'2021-03-31');
